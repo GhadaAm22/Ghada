bbadbd44a1d0d69985f89eca6b3b3bb9655fe9b0
ghada <ghada_alshahrani@hotmail.com>
2022-10-27 09:55:17 +0300

    Initial Commit

---
+++ Ghada/GhadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhadaApp.swift
+//  Ghada
+//
+//  Created by Ghada Amer Alshahrani on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct GhadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
